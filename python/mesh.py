2a8a76f5d5f6fa8629b7820f19d96e5a9e71a564
zoey.zyhu <zoey.zyhu@gmail.com>
2023-11-26 13:26:18 -0500

    Added pre-commit hooks and corrected styles

--- python/mesh.py
+++ python/mesh.py
@@ -1,14 +1,20 @@
-from mesh_block_tree import MeshBlockTree
+# pylint: disable = import-error, too-few-public-methods, unused-argument, unused-variable, unused-import, undefined-variable
+"""Mesh class and related functions."""
+
+from meshblock_tree import MeshBlockTree
 from coordinate_factory import CoordinateFactory
 from typing_extensions import Self
 import ray
 
+
 @ray.remote
-class MeshActor:
+class Mesh:
+    """A class representing a mesh."""
+
     def __init__(self, tree: MeshBlockTree, coordinate_type: str, nghost: int):
+        """Initialize Mesh with tree, coordinate type, and optional ghost zones."""
         mblocks = []
-        pass
+
 
 if __name__ == "__main__":
     mesh = Mesh(tree, "cartesian", 1)
-    pass
--- python/meshblock.py
+++ python/meshblock.py
@@ -1,12 +1,19 @@
+# pylint: disable = import-error, too-many-instance-attributes, too-many-branches, too-many-statements
+"""Module containing the MeshBlock class."""
+
+from typing import Any
+from typing_extensions import Self
 from numpy.lib.stride_tricks import as_strided
 from region_size import RegionSize
 from coordinate_factory import CoordinateFactory
-from typing import Any
-from typing_extensions import Self
 import numpy as np
 
+
 class MeshBlock:
+    """A class representing a mesh block."""
+
     def __init__(self, size: RegionSize, coordinate_type: str, nghost: int = 0):
+        """Initialize MeshBlock with size, coordinate type, and optional ghost zones."""
         self.coord = CoordinateFactory.create(size, coordinate_type, nghost)
         self.nvar = 0
         self.data = None
@@ -16,7 +23,7 @@ class MeshBlock:
         self.nx3 = size.nx3
         self.nx2 = size.nx2
         self.nx1 = size.nx1
-        
+
         if size.nx3 > 1:
             self.nc3 = size.nx3 + 2 * nghost
         else:
@@ -30,6 +37,7 @@ class MeshBlock:
         self.nc1 = size.nx1 + 2 * nghost
 
     def allocate(self, nvar: int = 1) -> Self:
+        """Allocate memory for the mesh block."""
         self.nvar = int(nvar)
         if self.nvar < 1:
             raise ValueError("nvar must be >= 1")
@@ -39,29 +47,38 @@ class MeshBlock:
         # interior slices
         for o3 in [-1, 0, 1]:
             for o2 in [-1, 0, 1]:
-                for o1 in [-1, 0, 1]: 
+                for o1 in [-1, 0, 1]:
                     nghost = self.coord.nghost
-                    if o1 == -1: offset1, len1 = nghost, nghost
-                    if o1 == 0: offset1, len1 = nghost, self.nx1
-                    if o1 == 1: offset1, len1 = self.nx1, nghost
+                    if o1 == -1:
+                        offset1, len1 = nghost, nghost
+                    elif o1 == 0:
+                        offset1, len1 = nghost, self.nx1
+                    else:
+                        offset1, len1 = self.nx1, nghost
 
                     if self.nx2 == 1:
                         nghost = 0
                     else:
                         nghost = self.coord.nghost
 
-                    if o2 == -1: offset2, len2 = nghost, nghost
-                    if o2 == 0: offset2, len2 = nghost, self.nx2
-                    if o2 == 1: offset2, len2 = self.nx2, nghost
+                    if o2 == -1:
+                        offset2, len2 = nghost, nghost
+                    elif o2 == 0:
+                        offset2, len2 = nghost, self.nx2
+                    else:
+                        offset2, len2 = self.nx2, nghost
 
                     if self.nx3 == 1:
                         nghost = 0
                     else:
                         nghost = self.coord.nghost
 
-                    if o3 == -1: offset3, len3 = nghost, nghost
-                    if o3 == 0: offset3, len3 = nghost, self.nx3
-                    if o3 == 1: offset3, len3 = self.nx3, nghost
+                    if o3 == -1:
+                        offset3, len3 = nghost, nghost
+                    elif o3 == 0:
+                        offset3, len3 = nghost, self.nx3
+                    else:
+                        offset3, len3 = self.nx3, nghost
 
                     byte = self.data.itemsize
 
@@ -70,38 +87,47 @@ class MeshBlock:
                                   offset2: offset2 + len2,
                                   offset1: offset1 + len1,
                                   0: self.nvar],
-                        shape = (len3, len2, len1, self.nvar),
-                        strides = (self.nc2 * self.nc1 * self.nvar * byte, self.nc1 *
-                                   self.nvar * byte, self.nvar * byte, byte),
-                        writeable = False
+                        shape=(len3, len2, len1, self.nvar),
+                        strides=(self.nc2 * self.nc1 * self.nvar * byte, self.nc1 *
+                                 self.nvar * byte, self.nvar * byte, byte),
+                        writeable=False
                     )
 
         # ghost zones
         for o3 in [-1, 0, 1]:
             for o2 in [-1, 0, 1]:
-                for o1 in [-1, 0, 1]: 
+                for o1 in [-1, 0, 1]:
                     nghost = self.coord.nghost
-                    if o1 == -1: offset1, len1 = 0, nghost
-                    if o1 == 0: offset1, len1 = nghost, self.nx1
-                    if o1 == 1: offset1, len1 = nghost + self.nx1, nghost
+                    if o1 == -1:
+                        offset1, len1 = 0, nghost
+                    elif o1 == 0:
+                        offset1, len1 = nghost, self.nx1
+                    else:
+                        offset1, len1 = nghost + self.nx1, nghost
 
                     if self.nx2 == 1:
                         nghost = 0
                     else:
                         nghost = self.coord.nghost
 
-                    if o2 == -1: offset2, len2 = 0, nghost
-                    if o2 == 0: offset2, len2 = nghost, self.nx2
-                    if o2 == 1: offset2, len2 = nghost + self.nx2, nghost
+                    if o2 == -1:
+                        offset2, len2 = 0, nghost
+                    elif o2 == 0:
+                        offset2, len2 = nghost, self.nx2
+                    else:
+                        offset2, len2 = nghost + self.nx2, nghost
 
                     if self.nx3 == 1:
                         nghost = 0
                     else:
                         nghost = self.coord.nghost
 
-                    if o3 == -1: offset3, len3 = 0, nghost
-                    if o3 == 0: offset3, len3 = nghost, self.nx3
-                    if o3 == 1: offset3, len3 = nghost + self.nx3, nghost
+                    if o3 == -1:
+                        offset3, len3 = 0, nghost
+                    elif o3 == 0:
+                        offset3, len3 = nghost, self.nx3
+                    else:
+                        offset3, len3 = nghost + self.nx3, nghost
 
                     byte = self.data.itemsize
 
@@ -110,95 +136,100 @@ class MeshBlock:
                                   offset2: offset2 + len2,
                                   offset1: offset1 + len1,
                                   0: self.nvar],
-                        shape = (len3, len2, len1, self.nvar),
-                        strides = (self.nc2 * self.nc1 * self.nvar * byte, self.nc1 * self.nvar * byte, self.nvar * byte, byte)
+                        shape=(len3, len2, len1, self.nvar),
+                        strides=(self.nc2 * self.nc1 * self.nvar * byte,
+                                 self.nc1 * self.nvar * byte, self.nvar * byte, byte)
                     )
         return self
 
     def part(self, offset: (int, int, int), of1: int, of2: int) -> np.ndarray:
-        pass
+        """Extract a part of the mesh block."""
 
     def fill_random(self, seed: int = 0) -> Self:
+        """Fill ghost zones with random values."""
         # set ghost zones to zero
         self.data[:] = -np.ones(self.data.shape)
         np.random.seed(seed)
-        self.ghost[(0,0,0)][:] = np.random.uniform(0, 1,
-            size = (self.nx3, self.nx2, self.nx1, self.nvar))
+        self.ghost[(0, 0, 0)][:] = np.random.uniform(0, 1,
+                                                     size=(self.nx3, self.nx2, self.nx1, self.nvar))
 
         return self
 
     def print_data(self) -> None:
+        """Print the data in the mesh block."""
         for n in range(self.nvar):
             print(f"var {n} = ")
             for k in range(self.nc3):
                 print(f"(z = {self.coord.x3v[k]:.2f})")
                 for j in range(self.nc2):
                     for i in range(self.nc1):
-                        print(f"{self.data[k, j, i, n]:.3f}, ", end = "")
+                        print(f"{self.data[k, j, i, n]:.3f}, ", end="")
                     print()
                 if k < self.nc3 - 1:
                     print()
 
     def __str__(self) -> str:
+        """Return a string representation of the mesh block."""
         if self.data is None:
             return f"{self.coord}\ndata=None"
-        else:
-            return f"{self.coord}\ndata_shape={self.data.shape}"
+        return f"{self.coord}\ndata_shape={self.data.shape}"
 
     def __eq__(self, other: Any) -> bool:
+        """Check if two MeshBlock instances are equal."""
         if isinstance(other, MeshBlock):
             return self.coord == other.coord and self.data == other.data
         return False
 
+
 if __name__ == "__main__":
     # test 1
-    rs = RegionSize(x1dim = (0, 1, 2), x2dim = (0, 1, 4))
+    rs = RegionSize(x1dim=(0, 1, 2), x2dim=(0, 1, 4))
     mb = MeshBlock(rs, "cartesian")
     mb.allocate()
     print(mb)
 
     # test 2
-    mb = MeshBlock(rs, "cylindrical", nghost = 2)
+    mb = MeshBlock(rs, "cylindrical", nghost=2)
     mb.allocate(2)
     print(mb)
 
     # test 3
-    rs1 = RegionSize(x1dim = (0, 40., 4), x2dim = (0, 40., 2))
-    mb1 = MeshBlock(rs1, "cartesian", nghost = 1)
+    rs1 = RegionSize(x1dim=(0, 40., 4), x2dim=(0, 40., 2))
+    mb1 = MeshBlock(rs1, "cartesian", nghost=1)
     mb1.allocate(2).fill_random()
     print("===== mb1 =====")
     mb1.print_data()
-    print(mb1.view[(0,0,1)][0,:,:,0])
-    print(mb1.view[(0,0,1)][0,:,:,1])
-    print(mb1.view[(0,-1,-1)][0,:,:,0])
-    print(mb1.view[(0,-1,-1)][0,:,:,1])
+    print(mb1.view[(0, 0, 1)][0, :, :, 0])
+    print(mb1.view[(0, 0, 1)][0, :, :, 1])
+    print(mb1.view[(0, -1, -1)][0, :, :, 0])
+    print(mb1.view[(0, -1, -1)][0, :, :, 1])
     print("===============")
 
-    rs2 = RegionSize(x1dim = (40., 120., 4), x2dim = (0, 40., 2))
-    mb2 = MeshBlock(rs2, "cartesian", nghost = 1)
+    rs2 = RegionSize(x1dim=(40., 120., 4), x2dim=(0, 40., 2))
+    mb2 = MeshBlock(rs2, "cartesian", nghost=1)
     mb2.allocate(2).fill_random()
     print("===== mb2 =====")
     mb2.print_data()
-    print(mb2.view[(0,1,0)][0,:,:,0])
-    print(mb2.view[(0,1,0)][0,:,:,1])
-    print(mb2.view[(0,-1,1)][0,:,:,0])
-    print(mb2.view[(0,-1,1)][0,:,:,1])
+    print(mb2.view[(0, 1, 0)][0, :, :, 0])
+    print(mb2.view[(0, 1, 0)][0, :, :, 1])
+    print(mb2.view[(0, -1, 1)][0, :, :, 0])
+    print(mb2.view[(0, -1, 1)][0, :, :, 1])
     print("===============")
 
-    rs3 = RegionSize(x1dim = (0., 40., 4), x2dim = (40, 120., 2))
-    mb3 = MeshBlock(rs3, "cartesian", nghost = 1)
+    rs3 = RegionSize(x1dim=(0., 40., 4), x2dim=(40, 120., 2))
+    mb3 = MeshBlock(rs3, "cartesian", nghost=1)
     mb3.allocate(2).fill_random()
     print("===== mb3 =====")
     mb3.print_data()
-    print(mb3.view[(0,1,-1)][0,:,:,0])
-    print(mb3.view[(0,1,-1)][0,:,:,1])
+    print(mb3.view[(0, 1, -1)][0, :, :, 0])
+    print(mb3.view[(0, 1, -1)][0, :, :, 1])
     print("===============")
 
-    rs4 = RegionSize(x1dim = (40., 120., 4), x2dim = (40, 120., 2))
-    mb4 = MeshBlock(rs4, "cartesian", nghost = 1)
+    rs4 = RegionSize(x1dim=(40., 120., 4), x2dim=(40, 120., 2))
+    mb4 = MeshBlock(rs4, "cartesian", nghost=1)
     mb4.allocate(2).fill_random()
     print("===== mb4 =====")
     mb4.print_data()
-    print(mb4.view[(0,1,1)][0,:,:,0])
-    print(mb4.view[(0,1,1)][0,:,:,1])
+    print(mb4.view[(0, 1, 1)][0, :, :, 0])
+    print(mb4.view[(0, 1, 1)][0, :, :, 1])
     print("===============")
