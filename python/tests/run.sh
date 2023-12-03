ba6419c8471e2fc9ddf92403ba7b32c45843e151
zoey.zyhu <zoey.zyhu@gmail.com>
2023-12-03 17:21:36 -0500

    wip

--- python/tests/run.sh
+++ python/tests/run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-log_file="log_actor_1203.txt"
 current_time=$(date +"%T")
+log_file="log_actor_1203_worker_id.txt"
 test_description="
     Test if a workder_id represent a unique logical CPU core. \n
     \t- Set num_cpus = 4 \n
     \t- Check if we only get 4 unique worker_ids \n
-    :: We get 11 worker_ids. \n\\n
-    Replace workder_id with actor_id and check the difference. \n\n
+    :: We get 11 worker_ids. \n\n
+    \t-Replace workder_id with actor_id and check the difference. \n
+    :: We get 11 actor_ids. \n\n
+    \t-In the decorator for the actor, set fractional num_cpu = 0.1. \n
+    :: Still 11 worker_ids. \n\n
 "
-separator="===================================================================================================="
+separator="\n\n\n\n\n===================================================================================================="
 
-echo "\n\n\n\n\n$separator" >> "$log_file"
-echo "Test started at: $current_time\n" >> "$log_file"
-echo $test_description >> "$log_file"
+echo -e "$separator" >> "$log_file"
+echo -e "Test started at: $current_time\n" >> "$log_file"
+echo -e $test_description >> "$log_file"
 python test_actor.py >> "$log_file"
